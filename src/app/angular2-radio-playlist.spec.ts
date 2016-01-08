873d740e3e88022d434b55dec57a720cc2d8a5dc
oegnus <tomasz.wiencek@gmail.com>
2016-01-08 16:30:45 +0100

    Added settings for visual-studio-code editor

--- src/app/angular2-radio-playlist.spec.ts
+++ src/app/angular2-radio-playlist.spec.ts
@@ -1,13 +1,9 @@
-import {describe, it, expect, beforeEachProviders, inject} from 'angular2/testing';
+import {describe, it, expect, beforeEachProviders, inject} from '../node_modules/angular2/testing';
 import {Angular2RadioPlaylistApp} from '../app/angular2-radio-playlist';
 
 beforeEachProviders(() => [Angular2RadioPlaylistApp]);
 
 describe('App: Angular2RadioPlaylist', () => {
-  it('should have the `defaultMeaning` as 42', inject([Angular2RadioPlaylistApp], (app) => {
-    expect(app.defaultMeaning).toBe(42);
-  }));
-
   describe('#meaningOfLife', () => {
     it('should get the meaning of life', inject([Angular2RadioPlaylistApp], (app) => {
       
